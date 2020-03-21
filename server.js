e800cc5602706fcf7d783f492c1444f4fd6d31b5
millord <juancarlosmillord@gmail.com>
2020-03-21 14:01:05 -0400

    more work in the server file

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require("http");
 
+const todos = ["hola", "tu"];
+const newTodos = JSON.stringify(todos);
+
 // creating a server
-const server = http.createServer(() => (req, res) => {
-  res.send();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(400, {
+    "Content-Type": "application/json"
+  });
+
+  res.end(
+    JSON.stringify({
+      success: false,
+      error: "Please add email",
+      data: null
+    })
+  );
 });
 
 const PORT = 8000;
